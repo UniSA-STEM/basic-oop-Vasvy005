6599593654a80ae7612f11d0b0f9ce362448d8d3
Vasvy005 <vashistvedant64@gmail.com>
2025-10-05 19:05:47 +1030

    Update Asset.py

--- Asset.py
+++ Asset.py
@@ -1,8 +1,23 @@
 """
-File: Asset.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+File: asset.py
+Description: Defines the Asset class. Assets represent digital items like 
+CryptoTokens, Data Spikes, and Security Chips. Assets can also be encrypted 
+to protect them from being transferred or stolen.
+Author: Vedant Vashist
+ID: 110315681
+Username: VASVY005
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+from dataclasses import dataclass
+
+@dataclass
+class Asset:
+    name: str
+    description: str
+    encrypted: bool = False
+
+    def __str__(self):
+        if self.encrypted:
+            return f"{self.name}: {self.description} [Encrypted]"
+        return f"{self.name}: {self.description}"
